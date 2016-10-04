4fa0185829de3ead9dc343ca96adb8199a649a86
jack33232 <jack33232@gmail.com>
2016-10-04 22:42:23 +0800

    - Small fix

--- src/Klein/Request.php
+++ src/Klein/Request.php
@@ -115,9 +115,9 @@ class Request
         $body = null
     ) {
         // Assignment city...
-        $this->params_get   = new  \Klien\DataCollection\RequestDataCollection($params_get);
-        $this->params_post  = new  \Klien\DataCollection\RequestDataCollection($params_post);
-        $this->cookies      = new  \Klien\DataCollection\RequestDataCollection($cookies);
+        $this->params_get   = new RequestDataCollection($params_get);
+        $this->params_post  = new RequestDataCollection($params_post);
+        $this->cookies      = new RequestDataCollection($cookies);
         $this->server       = new ServerDataCollection($server);
         $this->headers      = new HeaderDataCollection($this->server->getHeaders());
         $this->files        = new DataCollection($files);
